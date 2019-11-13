284f4fc249b2fb36ab86d6c2b21f74bb17a35fea
Do Buy <haykees.comet@live.com>
2019-11-13 11:44:53 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "King of Diamonds";
+var cardTwo = "King of Hearts";
+var cardThree = "Queen of Diamonds";
+var cardFour = "Queen of Hearts";
+
+console.log("You flipped the " + cardOne);
+console.log("You flipped " + cardThree);
