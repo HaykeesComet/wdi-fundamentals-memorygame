155497a812510bd60623b2117e2d372bb5b8bbc7
Do Buy <haykees.comet@live.com>
2019-12-04 19:15:31 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,3 +7,21 @@ var cardFour = "Queen of Hearts";
 
 console.log("You flipped the " + cardOne);
 console.log("You flipped " + cardThree);
+
+const cards = ['queen', 'queen', 'king', 'king'];
+
+cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("You flipped " + cardsInPlay);
+
+cardsInPlay = [];
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("You flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
